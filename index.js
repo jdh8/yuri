aea6762d33ae8a8be62b2a9ce8957bde64f1911f
Owen McGrath <owenmcgrath111@outlook.com>
2018-03-15 20:28:59 -0500

    Add client events

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Commando = require('discord.js-commando')
 const sqlite = require('sqlite')
 const path = require('path')
+const {oneLine} = require('common-tags')
 
 const client = new Commando.Client({
   commandPrefix: 'yuri',
@@ -10,8 +11,46 @@ const client = new Commando.Client({
 })
 
 client
+  .on('error', console.error)
+  .on('warn', console.warn)
+  .on('debug', console.log)
   .on('ready', () => {
     console.log(`Client logged in as ${client.user.tag} (${client.user.id})`)
+  })
+  .on('disconnect', () => {
+    console.warn('Disconnected!')
+  })
+  .on('reconnecting', () => {
+    console.warn('Reconnecting...')
+  })
+  .on('commandError', (cmd, err) => {
+    if (err instanceof Commando.FriendlyError) return
+    console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err)
+  })
+  .on('commandBlocked', (msg, reason) => {
+    console.log(oneLine`
+      Command ${msg.command ? `${msg.command.groupID}` : ''} blocked; ${reason}
+    `)
+  })
+  .on('commandPrefixChange', (guild, prefix) => {
+    console.log(oneLine`
+      Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'default'}`}
+      ${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+    `)
+  })
+  .on('commandStatusChange', (guild, command, enabled) => {
+    console.log(oneLine`
+      Command ${command.groupID}:${command.memberName}
+      ${enabled ? 'enabled' : 'disabled'}
+      ${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+    `)
+  })
+  .on('groupStatusChange', (guild, group, enabled) => {
+    console.log(oneLine`
+      Group ${group.id}
+      ${enabled ? 'enabled' : 'disabled'}
+      ${guild ? `in guild ${guild.aname} (${guild.id})` : 'globally'}.
+    `)
   })
 
 client.setProvider(
