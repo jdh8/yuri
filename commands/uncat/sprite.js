5ba2eb304a08b8edf51b4f812984856d152493ee
Owen McGrath <owenmcgrath111@outlook.com>
2018-03-20 06:53:52 -0500

    Rework sprite command

--- commands/uncat/sprite.js
+++ commands/uncat/sprite.js
@@ -2,10 +2,19 @@ const Discord = require('discord.js')
 const Commando = require('discord.js-commando')
 const Jimp = require('jimp')
 const fs = require('fs')
+const util = require('util')
+const {stripIndent} = require('common-tags')
+
+Jimp.prototype.getBufferAsync = util.promisify(Jimp.prototype.getBuffer)
 
 const faces = fs.readdirSync('assets/sprite/face').map(face =>
-  face.slice(5, -4)
+  face.slice(0, -4)
 )
+const poses = {
+  normal: ['normal', 'normal'],
+  right: ['normal', 'hand'],
+  both: ['hand', 'hand']
+}
 
 module.exports = class SpriteCommand extends Commando.Command {
   constructor (client) {
@@ -14,47 +23,68 @@ module.exports = class SpriteCommand extends Commando.Command {
       group: 'uncat',
       memberName: 'sprite',
       description: 'Composites a Yuri sprite',
-      details: ``,
+      details: stripIndent`
+        Valid faces: ${faces.map(face => `\`${face}\``).join(', ')}
+        Valid hand positions: ${
+          Object.getOwnPropertyNames(poses)
+            .map(pose => `\`${pose}\``)
+            .join(', ')
+        }
+      `,
       args: [
         {
           key: 'face',
-          prompt: 'What should my expression be? (normal or sprite name)',
+          prompt: stripIndent`
+            What should my expression be?
+            Valid faces: ${faces.map(face => `\`${face}\``).join(', ')}
+          `,
           type: 'string',
           validate: value => faces.indexOf(value.toLowerCase()) >= 0,
           parse: value => value.toLowerCase()
         },
         {
-          key: 'hands',
-          prompt: 'Which hands should I have up? (none, right or both)',
+          key: 'pose',
+          prompt: stripIndent`
+            How should I have hands?
+            Valid hand positions: ${
+              Object.getOwnPropertyNames(poses)
+                .map(pose => `\`${pose}\``)
+                .join(', ')
+            }
+          `,
+          label: 'hand position',
           type: 'string',
-          validate: value => /none|right|both/i.test(value),
-          parse: value => [/both/i.test(value), /both|right/i.test(value)]
+          validate: value => poses.hasOwnProperty(value.toLowerCase()),
+          parse: value => value.toLowerCase()
         },
         {
           key: 'casual',
-          prompt: 'Should I wear my casual clothes? (yes for casual)',
+          prompt: 'Should I wear my casual clothes?\n(`yes` for casual)',
           type: 'string',
-          validate: value => /yes|casual|no|school|uniform/i.test(value),
-          parse: value => /yes|casual/i.test(value)
+          validate: value => /yes|cas|casual|no|school|uniform/i.test(value),
+          parse: value => /yes|casual|cas/i.test(value)
         }
       ]
     })
   }
   async run (message, args) {
-    message.channel.send('Compositing...')
+    message.reply('compositing...')
 
-    const left = `assets/sprite/left/left${args.casual ? '-cas' : ''}${args.hands[0] ? '-hand' : ''}.png`
-    const face = `assets/sprite/face/face-${args.face}.png`
-    const right = `assets/sprite/right/right${args.casual ? '-cas' : ''}${args.hands[1] ? '-hand' : ''}.png`
+    const casual = args.casual ? '-cas' : ''
 
-    Jimp.read(left).then(async image => {
-      image
+    const face = `assets/sprite/face/${args.face}.png`
+    const left = `assets/sprite/left/${poses[args.pose][0]}${casual}.png`
+    const right = `assets/sprite/right/${poses[args.pose][1]}${casual}.png`
+
+    const buffer = await Jimp.read(left).then(async image => {
+      return image
         .composite(await Jimp.read(face), 0, 0)
         .composite(await Jimp.read(right), 0, 0)
-        .getBuffer('image/png', (err, buffer) => {
-          if (err) throw err
-          message.reply('', new Discord.MessageAttachment(buffer, 'yuri.png'))
-        })
+        .getBufferAsync('image/png')
     })
+
+    const attachment = new Discord.MessageAttachment(buffer, 'yuri.png')
+
+    return message.reply('', attachment)
   }
 }
