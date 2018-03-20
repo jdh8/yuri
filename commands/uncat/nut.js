d5bb3bfa92cc54de363a28eabf3dfb1e5260a8b8
Owen McGrath <owenmcgrath111@outlook.com>
2018-03-20 18:04:28 -0500

    Add nut command

---
+++ commands/uncat/nut.js
@@ -0,0 +1,30 @@
+const Commando = require('discord.js-commando')
+const {oneLine} = require('common-tags')
+
+module.exports = class NutCommand extends Commando.Command {
+  constructor (client) {
+    super(client, {
+      name: 'nut',
+      group: 'uncat',
+      memberName: 'nut',
+      description: 'Nut on someone or something',
+      details: 'If the object starts with "in", that will replace "on"',
+      examples: ['yuri nut the floor', 'yuri nut in a bucket'],
+      args: [
+        {
+          key: 'object',
+          label: 'object to nut on',
+          prompt: 'What would you like to nut on?',
+          type: 'string'
+        }
+      ]
+    })
+  }
+  async run (message, args) {
+    return message.channel.send(oneLine`
+      ${message.author.toString()} nuts
+      ${/^in/i.test(args.object) ? 'in' : 'on'}
+      ${args.object.replace(/^[io]n/i, '')}.
+    `)
+  }
+}
