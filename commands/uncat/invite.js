c641d90f2103a5e0e0905a6467104001f5ed8666
Owen McGrath <owenmcgrath111@outlook.com>
2018-03-24 16:16:57 -0500

    Add invite command

---
+++ commands/uncat/invite.js
@@ -0,0 +1,20 @@
+const Commando = require('discord.js-commando')
+const {stripIndent} = require('common-tags')
+
+module.exports = class InviteCommand extends Commando.Command {
+  constructor (client) {
+    super(client, {
+      name: 'invite',
+      aliases: ['invite-link'],
+      group: 'uncat',
+      memberName: 'invite',
+      description: 'Generates an invite link for KnifeWife'
+    })
+  }
+  async run (message, args) {
+    return message.reply(stripIndent`
+      here is your invite link:
+      https://discordapp.com/oauth2/authorize?&client_id=407652636054257665&scope=bot
+    `)
+  }
+}
