4d3a9c41ea415226711d190f55dec1ecd15aa526
Owen McGrath <owenmcgrath111@outlook.com>
2018-04-10 20:50:24 -0500

    feat: Hug command
    Add a command to hug a mentioned user
    
    undefined

---
+++ commands/fun/hug.js
@@ -0,0 +1,38 @@
+const Commando = require('discord.js-commando')
+const {oneLineCommaListsAnd} = require('common-tags')
+
+module.exports = class extends Commando.Command {
+  constructor (client) {
+    super(client, {
+      name: 'hug',
+      group: 'fun',
+      memberName: 'hug',
+      description: 'Mention someone to hug',
+      examples: ['yuri hug @user'],
+      args: [
+        {
+          key: 'person',
+          label: 'mention',
+          prompt: 'What would you like to hug on? (mention one or more users)',
+          type: 'string',
+          validate: person => /^((<@[!&]?\d+>|@everyone|@here)\s*)+$/.test(person),
+          parse: person => person.replace(/\s/g, ',').split(',')
+        }
+      ]
+    })
+  }
+  async run (message, args) {
+    return message.channel.send('', {
+      embed: {
+        description: oneLineCommaListsAnd`
+          ${message.author.toString()} hugs
+          ${args.person}
+        `,
+        image: {
+          url: 'https://cdn.discordapp.com/attachments/403697069857964042/432740154621427712/yuri-hug.png'
+        },
+        color: 0x9800FF
+      }
+    })
+  }
+}
