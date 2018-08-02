f43fa690d57bcd9645800ad95efbe841fd2b3da4
Owen McGrath <owenmcgrath111@outlook.com>
2018-08-02 17:08:25 -0500

    feat: Allow commands to have events
    Allowed commands to have events associated events, that are added to the
    client when the command is added. This is done to allow commands and
    events that go with them to be able to be "put together"
    
    undefined

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,13 @@
+const {Collection} = require('discord.js')
+
 module.exports = {
   name: 'test',
   group: 'test',
   run ({message, args}) {
     console.log('Running command!')
     message.reply('Test command')
-  }
+  },
+  events: new Collection().set('messageDelete', function () {
+    this.emit('warn', 'A message was deleted!')
+  })
 }
