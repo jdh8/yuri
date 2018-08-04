beb8d11870a3144c5555046316bae7c71ebe7eb8
Owen McGrath <owenmcgrath111@outlook.com>
2018-08-04 16:15:23 -0500

    refactor: Make groups more explicit
    Groups now must be added before commands are put into them. Added a
    method for adding groups required by default commands.
    
    undefined

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const {KwClient} = require('./lib/client')
 const commands = require('./commands')
 
-const client = new KwClient({
-})
+const client = new KwClient()
+client.addDefaultGroups()
 client.addDefaultEvents()
 client.addDefaultCommands()
+client.addGroups([
+  ['interjections', 'Interjections', '*Hnnnnng*'],
+  ['test', 'Test', '`test`']
+])
 client.addCommands(commands)
 
 client.login(process.env.KNIFEWIFE_TOKEN)
