2ea79edc6a812f47854a45b3a752f4b023bb1569
Owen McGrath <owenmcgrath111@outlook.com>
2018-08-09 21:28:32 -0500

    refactor: Add bot info to client
    undefined

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const {KwClient} = require('./lib')
 const commands = require('./commands')
 
-const client = new KwClient()
+const client = new KwClient({
+  info: {
+    name: 'KnifeWife',
+    desc: 'A Yuri-themed Discord bot',
+    version: '1.0.0-alpha',
+    developer: 'owm#1337',
+    owner: '212212754033934336',
+    repo: 'https://github.com/owm111/knife-wife'
+  }
+})
 client.addDefaults()
 client.commands.groups.add([
   ['interjections', 'Interjections', '*Hnnnnng*'],
