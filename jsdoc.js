0512b688f7a2732c79ee25834dc93d1ce8dec75d
Owen McGrath <owenmcgrath111@outlook.com>
2018-09-01 08:51:10 -0500

    docs(fw): Update JSDoc config
    [ci skip]
    
    Added more options to the JSDoc config file. Added more docs-related
    scripts to package.json
    
    undefined

--- jsdoc.js
+++ jsdoc.js
@@ -2,6 +2,16 @@ module.exports = {
   plugins: [
     'plugins/markdown'
   ],
+  source: {
+    include: ['lib'],
+    includePattern: '.+\\.m?js(doc|x)?$'
+  },
+  sourceType: 'module',
+  opts: {
+    destination: './docs/',
+    readme: './README.md',
+    recurse: true
+  },
   templates: {
     cleverLinks: true
   },
